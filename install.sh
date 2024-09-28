0c1f691c2460b47fe543749ad2fda00005a00a8d
Matt Pace <matthew.dp.96@gmail.com>
2024-09-28 14:39:59 -0400

    Install JetBrainsMono NerdFont

--- install.sh
+++ install.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-# ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+install_common() {
+  # ohmyzsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+install_darwin() {
+  brew install --cask font-jetbrains-mono-nerd-font
+}
+
+install_linux() {
+  wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip &&
+    cd ~/.local/share/fonts &&
+    unzip JetBrainsMono.zip &&
+    rm JetBrainsMono.zip &&
+    fc-cache -fv
+
+  cd -
+}
+
+install_common
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  install_darwin
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  install_linux
+else
+  echo "Unknown OS."
+  exit 1
+fi
