913ca96babd34467a353cb27a8818d3552fb4b73
Matt Pace <matthew.dp.96@gmail.com>
2024-09-28 14:02:02 -0400

    Manage zsh plugins with git submodule

--- install.sh
+++ install.sh
@@ -2,7 +2,3 @@
 
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
