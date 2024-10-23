1bd6a830395516d44123beeb772a50a94a751100
Matt Pace <matt.pace@tidelift.com>
2024-10-23 11:59:25 -0400

    lazyvim: change ruby theme

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -3,7 +3,16 @@ return {
   lazy = false,
   priority = 1000,
   ---@type solarized.config
-  opts = {},
+  opts = {
+    on_highlights = function(colors, color)
+      ---@type solarized.highlights
+      local groups = {
+        ["@string.special.symbol"] = { fg = colors.violet },
+      }
+
+      return groups
+    end,
+  },
   config = function(_, opts)
     vim.o.termguicolors = true
     vim.o.background = "light"
